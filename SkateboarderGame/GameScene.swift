5953003a3c9bea946ad574ef834a5659085f1f6d
Anna Belousova <anna.belousova.2011@gmail.com>
2021-03-20 13:32:56 +0400

    Added background and skater

--- SkateboarderGame/GameScene.swift
+++ SkateboarderGame/GameScene.swift
@@ -9,9 +9,25 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+
+	let skater = Skater(imageNamed: "skater")
     
     override func didMove(to view: SKView) {
+		anchorPoint = CGPoint.zero
+
+		let background = SKSpriteNode(imageNamed: "background")
+		background.position = CGPoint(x: frame.midX, y: frame.midY)
+		addChild(background)
+
+		resetSkater()
+		addChild(skater)
     }
+
+	func resetSkater() {
+		skater.position = CGPoint(x: frame.midX/2.0, y: skater.frame.height/2.0 + 64.0)
+		skater.zPosition = 10
+		skater.minimumY = skater.frame.height/2.0 + 64.0
+	}
     
     override func update(_ currentTime: TimeInterval) {
     }
---
+++ SkateboarderGame/Skater.swift
@@ -0,0 +1,15 @@
+//
+//  Skater.swift
+//  SkateboarderGame
+//
+//  Created by Anna Belousova on 20.03.2021.
+//
+
+import SpriteKit
+
+class Skater: SKSpriteNode {
+	let velocity = CGPoint.zero
+	var minimumY: CGFloat = 0.0
+	let jumpSpeed: CGFloat = 20.0
+	let isOnGroung = true
+}
